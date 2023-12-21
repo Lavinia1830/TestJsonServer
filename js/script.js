5b54948562d8dadf40e0d583ab57c279c2516c5a
Lavinia1830 <lavinia.baratti@gmail.com>
2023-12-21 11:31:50 +0100

    Aggiornamento

--- js/script.js
+++ js/script.js
@@ -1,11 +1,33 @@
-const url = 'http://localhost:3000/users/';
+const url = 'http://localhost:3000/';
 
 //funzione per il click al bottone registrati
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('#registrazione').addEventListener('click', () => {
+
+    let btnRegistrazione = document.querySelector('#registrazione');
+    let btnLogin = document.querySelector('#login');
+
+    if(btnRegistrazione){
+        btnRegistrazione.addEventListener('click', singUp);
+    } else if (btnLogin){
+        btnLogin.addEventListener('click', login)
+    }
+
+
+    /* document.querySelector('#registrazione').addEventListener('click', () => {
         singUp();
-    })
+    })  forma contratta*/ 
 })
+
+//Funzione per vedere nella console gli oggetti 
+const getAll = () => {
+    fetch(url)
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(err => console.log(err));
+}
+getAll();
+
+
 
 //Funzione per la registrazione
 function singUp (){
@@ -20,7 +42,7 @@ function singUp (){
         email: email,
         password: password
     }
-    fetch(url, {
+    fetch(url + 'users', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(obj)})
@@ -28,6 +50,7 @@ function singUp (){
         .then(json => console.log(json))
         .catch(err => console.log(err));
 }
+
 
 
 //questa costante fa si che la password viene criptata 
@@ -42,4 +65,23 @@ const update = (id) => {
             .then(response => response.json())
             
     })
+}
+
+
+
+function login (){
+    let email = document.querySelector('#email').value;
+    let password = document.querySelector('#password').value;
+
+    let obj = {
+        email: email,
+        password: password
+    }
+    fetch(url + 'login', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(obj)})
+        .then(response => response.json())
+        .then(json => console.log(json))
+        .catch(err => console.log(err));
 }
